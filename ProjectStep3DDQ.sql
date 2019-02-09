c22bdd09403da0c7eedad08ff22fdcc4cdcee113
ottleyf <38929449+ottleyf@users.noreply.github.com>
2019-02-08 20:24:41 -0700

    add data correct relationships

--- ProjectStep3DDQ.sql
+++ ProjectStep3DDQ.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: classmysql.engr.oregonstate.edu:3306
--- Generation Time: Feb 08, 2019 at 03:38 PM
+-- Generation Time: Feb 08, 2019 at 07:22 PM
 -- Server version: 10.1.22-MariaDB
 -- PHP Version: 7.0.33
 
@@ -39,6 +39,18 @@ CREATE TABLE `laptops` (
   `ram` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `laptops`
+--
+
+INSERT INTO `laptops` (`Id`, `make`, `model`, `sn`, `purchase_date`, `warranty_end_date`, `cpu`, `ram`) VALUES
+(1, 'Dell', 'E6530', '3245323134', '2017-06-06', '2020-06-06', 'i5 3050', 8),
+(4, 'Dell', 'E7550', 'adfkei94234', '2017-06-12', '2020-06-12', 'i7 3050', 8),
+(5, 'Dell', 'E5637', '415456457ad', '2015-01-06', '2018-01-06', 'i5 1010', 4),
+(6, 'Dell', 'E5530', '32jgjl4lklj', '2011-06-29', '2014-06-29', 'i3 930', 2),
+(7, 'HP', 'ProBook 445', 'jleiwl3523', '2019-01-02', '2023-01-02', 'i7 9090', 16),
+(8, 'HP', 'Elite X2', 'fk3983kgls84', '2018-12-25', '2023-12-25', 'Ryzen 3', 8);
+
 -- --------------------------------------------------------
 
 --
@@ -51,6 +63,22 @@ CREATE TABLE `laptops_laptopdocs` (
   `doc_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `laptops_laptopdocs`
+--
+
+INSERT INTO `laptops_laptopdocs` (`Id`, `lt_id`, `doc_id`) VALUES
+(2, 1, 1),
+(1, 1, 2),
+(10, 4, 1),
+(9, 4, 2),
+(12, 5, 1),
+(11, 5, 2),
+(14, 6, 1),
+(13, 6, 2),
+(15, 7, 3),
+(16, 8, 3);
+
 -- --------------------------------------------------------
 
 --
@@ -62,6 +90,15 @@ CREATE TABLE `laptop_docs` (
   `title` varchar(100) NOT NULL,
   `doc_link` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `laptop_docs`
+--
+
+INSERT INTO `laptop_docs` (`Id`, `title`, `doc_link`) VALUES
+(1, 'How to Reset Your Windows 10 PC', 'https://www.laptopmag.com/articles/reset-windows-10-pc'),
+(2, 'Diagnostic Error Code Standardization For 2015 & 2016 Notebook and Tablet Systems', 'https://www.dell.com/support/article/us/en/04/sln300723/diagnostic-error-code-standardization-for-2015-2016-notebook-and-tablet-systems?lang=en'),
+(3, 'HP Notebook PCs - Troubleshooting Error Messages on a Black Screen that may Occur During Startup or ', 'https://support.hp.com/us-en/document/c00480483');
 
 -- --------------------------------------------------------
 
@@ -77,6 +114,16 @@ CREATE TABLE `location` (
   `zip` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `location`
+--
+
+INSERT INTO `location` (`Id`, `street_address`, `city`, `state`, `zip`) VALUES
+(1, '12345 Main Street', 'Boise', 'ID', '83706'),
+(2, '9875 First Ave', 'Salt Lake City', 'UT', '85487'),
+(3, '9924 Front Street', 'Hailey', 'ID', '85687'),
+(4, '5554 Avenue A', 'Seattle', 'WA', '95874');
+
 -- --------------------------------------------------------
 
 --
@@ -91,6 +138,20 @@ CREATE TABLE `peripherals` (
   `assigned_user` int(11) NOT NULL,
   `asset_tag` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `peripherals`
+--
+
+INSERT INTO `peripherals` (`Id`, `equip_type`, `make`, `model`, `assigned_user`, `asset_tag`) VALUES
+(2, 'Mouse', 'Dell', '', 13, 101),
+(3, 'Keyboard', '103', 'kb234', 13, 0),
+(6, 'Webcam', 'Microsoft', 'WC873', 13, 104),
+(7, 'Keyboard', '103', 'kb234', 13, 103),
+(8, 'Monitor', 'HP', 'ND3402', 13, 106),
+(9, 'Monitor', 'HP', '16gh', 13, 109),
+(10, 'Dock', 'Plugable', 'DC-100', 13, 120),
+(11, 'Keyboard', 'Logitec', 'LKB372', 7, 119);
 
 -- --------------------------------------------------------
 
@@ -109,6 +170,17 @@ CREATE TABLE `users` (
   `home_office` int(11) NOT NULL,
   `assigned_laptop` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`Id`, `first_name`, `last_name`, `department`, `job_title`, `pref_phone`, `pref_email`, `home_office`, `assigned_laptop`) VALUES
+(7, 'Felicia', 'Ottley', 'IT', 'SharePoint Administrator', '208-555-1234', 'ottleyf@oregonstate.edu', 2, 5),
+(10, 'Bob', 'Builder', 'Operations', 'Foreman', '555-55-1234', 'bob@letsbuildit.com', 2, 1),
+(11, 'Mary', 'Contrary', 'Sales', 'Coordinator', '555-77-6666', 'mary@quite.com', 1, 4),
+(12, 'Greg', 'Merlot', 'Executive', 'CEO', '999-11-1111', 'Greg@imaceo.com', 4, 6),
+(13, 'Bill', 'Stone', 'Sales', 'Sales Executive', '123-45-6789', 'bill@pebkac.com', 3, 7);
 
 --
 -- Indexes for dumped tables
@@ -169,37 +241,37 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `laptops`
 --
 ALTER TABLE `laptops`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `laptops_laptopdocs`
 --
 ALTER TABLE `laptops_laptopdocs`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `laptop_docs`
 --
 ALTER TABLE `laptop_docs`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `location`
 --
 ALTER TABLE `location`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `peripherals`
 --
 ALTER TABLE `peripherals`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Constraints for dumped tables
@@ -213,12 +285,17 @@ ALTER TABLE `laptops_laptopdocs`
   ADD CONSTRAINT `laptops_laptopdocs_ibfk_2` FOREIGN KEY (`doc_id`) REFERENCES `laptop_docs` (`Id`);
 
 --
+-- Constraints for table `peripherals`
+--
+ALTER TABLE `peripherals`
+  ADD CONSTRAINT `peripherals_ibfk_1` FOREIGN KEY (`assigned_user`) REFERENCES `users` (`Id`);
+
+--
 -- Constraints for table `users`
 --
 ALTER TABLE `users`
   ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`home_office`) REFERENCES `location` (`Id`),
-  ADD CONSTRAINT `users_ibfk_2` FOREIGN KEY (`assigned_laptop`) REFERENCES `laptops` (`Id`),
-  ADD CONSTRAINT `users_ibfk_3` FOREIGN KEY (`Id`) REFERENCES `peripherals` (`assigned_user`);
+  ADD CONSTRAINT `users_ibfk_2` FOREIGN KEY (`assigned_laptop`) REFERENCES `laptops` (`Id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
