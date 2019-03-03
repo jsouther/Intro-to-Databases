a5da0cca949395ec80642a5c3fb7ed61df4c6f67
jsouther <jacob.souther@hotmail.com>
2019-03-03 12:29:20 -0700

    Populate add user location options
    Add user location field now lists all locations in db

--- Database_Website/users.js
+++ Database_Website/users.js
@@ -13,15 +13,27 @@ module.exports = function(){
       });
     }
 
+   function getLocation(res, mysql, context, complete){
+        mysql.pool.query("SELECT Id, city FROM location", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.location = results;
+            complete();
+        });
+    }
+
     router.get('/', function(req,res){
       var callbackCount = 0;
       var context = {};
       //context.jsscripts = []
       var mysql = req.app.get('mysql');
       getUsers(res, mysql, context, complete);
+      getLocation(res, mysql, context, complete);
       function complete(){
         callbackCount++;
-        if(callbackCount >= 1){
+        if(callbackCount >= 2){
           res.render('users', context);
         }
       }
