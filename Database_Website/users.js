b3121b7fe6e425584cef231f03fecb2ca6c461e2
jsouther <jsouther@users.noreply.github.com>
2019-03-06 16:50:34 -0700

    Update User Functionality
    Update user now works

--- Database_Website/users.js
+++ Database_Website/users.js
@@ -2,6 +2,7 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
+    /* Get user info to display table of users*/
     function getUsers(res, mysql, context, complete){
       mysql.pool.query("SELECT users.Id, first_name, last_name, department, job_title, pref_phone, pref_email, location.city, location.state FROM users INNER JOIN location ON location.Id = users.home_office", function(error, results, fields){
         if(error){
@@ -13,6 +14,7 @@ module.exports = function(){
       });
     }
 
+    /*Get location info to display locations in dropdown menus*/
    function getLocations(res, mysql, context, complete){
         mysql.pool.query("SELECT Id, city FROM location", function(error, results, fields){
             if(error){
@@ -24,6 +26,7 @@ module.exports = function(){
         });
     }
 
+    /*Get specific user info for person of updating user*/
     function getUser(res, mysql, context, Id, complete){
         var sql = "SELECT Id, first_name, last_name, department, job_title, pref_phone, pref_email, home_office FROM users WHERE Id = ?";
         var inserts = [Id];
@@ -37,6 +40,7 @@ module.exports = function(){
         });
     }
 
+    /*Route to display all users and populate location dropdowns*/
     router.get('/', function(req,res){
       var callbackCount = 0;
       var context = {};
@@ -52,24 +56,22 @@ module.exports = function(){
       }
     });
 	
-	router.post('', function(req, res) {
-	var mysql = req.app.get('mysql');
-	var sql = "INSERT INTO users(`first_name`,`last_name`,`department`,`job_title`,`pref_phone`,`pref_email`,`home_office`) VALUES (?,?,?,?,?,?,?)";
-	var inserts = [req.body.fname_input, req.body.lname_input, req.body.department_input, req.body.title_input, req.body.phone_input, req.body.email_input, req.body.location] 
-			sql = mysql.pool.query(sql,inserts,function(error, results, fields){
-		if(error){
-			res.write(JSON.stringify(error));
-			res.end();
-		}else{
-			res.redirect('/users');
-		}
-	});
-
-	  
-	  
+    /*Route to add a user, redirects to users page*/
+  	router.post('', function(req, res) {
+    	var mysql = req.app.get('mysql');
+    	var sql = "INSERT INTO users(`first_name`,`last_name`,`department`,`job_title`,`pref_phone`,`pref_email`,`home_office`) VALUES (?,?,?,?,?,?,?)";
+    	var inserts = [req.body.fname_input, req.body.lname_input, req.body.department_input, req.body.title_input, req.body.phone_input, req.body.email_input, req.body.location] 
+    			sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+    		if(error){
+    			res.write(JSON.stringify(error));
+    			res.end();
+    		}else{
+    			res.redirect('/users');
+    		}
+  	  });
     });
 	
-
+    /*Route to URL to display one user for updating*/
     router.get('/:Id', function(req, res){
         callbackCount = 0;
         var context = {};
@@ -84,6 +86,25 @@ module.exports = function(){
             }
 
         }
+    });
+
+    /*Route to URL that update data is sent in order to update a user*/
+    router.put('/:Id', function(req, res){
+        var mysql = req.app.get('mysql');
+        console.log(req.body)
+        console.log(req.params.Id)
+        var sql = "UPDATE users SET first_name=?, last_name=?, department=?, job_title=?, pref_phone=?, pref_email=?, home_office=? WHERE Id=?";
+        var inserts = [req.body.first_name, req.body.last_name, req.body.department, req.body.job_title, req.body.pref_phone, req.body.pref_email, req.body.home_office, req.params.Id];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                console.log(error)
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.status(200);
+                res.end();
+            }
+        });
     });
 
     return router;
