e6adc19c1c8ffb01810c5e6ff77ac54fd7da6307
jsouther <jacob.souther@hotmail.com>
2019-03-19 00:08:05 -0600

    Update peripheral/laptop form validation
    Alerts user when trying to submit empty required fields.

--- Database_Website/public/js/updateLaptop.js
+++ Database_Website/public/js/updateLaptop.js
@@ -4,12 +4,37 @@
 ************************************************************/
 
 function updateLaptop(Id){
-    $.ajax({
-        url: '/laptops/' + Id,
-        type: 'PUT',
-        data: $('#update-laptop').serialize(),
-        success: function(result){
-            window.location.replace("./");
-        }
-    })
+
+  var modelValue = $('#model').val();
+  var serialValue = $('#sn').val();
+  var cpuValue = $('#cpu').val();
+  var ramValue = $('#ram').val();
+  let alertStrings = [];
+
+  if(modelValue === '') {
+    alertStrings.push('Please enter a model!');
+  }
+  if(serialValue === '') {
+    alertStrings.push('Please enter a serial number!');
+  }
+  if(cpuValue === '') {
+    alertStrings.push('Please enter a cpu!');
+  }
+  if(ramValue === '') {
+    alertStrings.push('Please enter an amount of ram!');
+  }
+
+  if(alertStrings.length > 0) {
+    alert(alertStrings.join('\n'));
+    return;
+  }
+
+  $.ajax({
+      url: '/laptops/' + Id,
+      type: 'PUT',
+      data: $('#update-laptop').serialize(),
+      success: function(result){
+          window.location.replace("./");
+      }
+  })
 };
--- Database_Website/public/js/updatePeripheral.js
+++ Database_Website/public/js/updatePeripheral.js
@@ -3,13 +3,31 @@
 ** Date: 3/9/19
 ************************************************************/
 
+
 function updatePeripheral(Id){
-    $.ajax({
-        url: '/peripherals/' + Id,
-        type: 'PUT',
-        data: $('#update-peripheral').serialize(),
-        success: function(result){
-            window.location.replace("./");
-        }
-    })
+
+  var makeValue = $('#make').val();
+  var modelValue = $('#model').val();
+  let alertStrings = [];
+
+  if(makeValue === '') {
+    alertStrings.push('Please enter a make!');
+  }
+  if(modelValue === '') {
+    alertStrings.push('Please enter a model!');
+  }
+
+  if(alertStrings.length > 0) {
+    alert(alertStrings.join('\n'));
+    return;
+  } 
+
+  $.ajax({
+      url: '/peripherals/' + Id,
+      type: 'PUT',
+      data: $('#update-peripheral').serialize(),
+      success: function(result){
+          window.location.replace("./");
+      }
+  })
 };
